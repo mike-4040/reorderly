f38014367b06ff56ae327ca5e6440d09660db761
Mike Kravtsov <kravtsovmike@gmail.com>
2025-11-23 21:55:50 -0800

    Deploy ignore (#21)
    * chore: update firebase.json to refine ignored files and emulator settings
    
    * refactor: update email handling to use verification before updating

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,3 @@
 // OAuth endpoints
 export { squareAuthorize } from './oauth/square/authorize';
 export { squareCallback } from './oauth/square/callback';
-
-// User management endpoints
-export { setEmail } from './users/endpoints/set-email';
